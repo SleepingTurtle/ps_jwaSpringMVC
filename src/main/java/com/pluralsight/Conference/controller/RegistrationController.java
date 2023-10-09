42ba50ff23a29ebf945684982233cab71ebc0b46
Turtle <josh@asleepingturtle.com>
2023-10-09 15:15:49 -0400

    Adding Validation

--- src/main/java/com/pluralsight/Conference/controller/RegistrationController.java
+++ src/main/java/com/pluralsight/Conference/controller/RegistrationController.java
@@ -1,7 +1,9 @@
 package com.pluralsight.Conference.controller;
 
 import com.pluralsight.Conference.model.Registration;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +18,13 @@ public class RegistrationController {
     }
 
     @PostMapping("registration")
-    public String addRegistration(@ModelAttribute ("registration") Registration registration) {
+    public String addRegistration(@Valid @ModelAttribute ("registration") Registration registration,
+                                  BindingResult result) {
+        if(result.hasErrors()) {
+            System.out.println("There were errors");
+            return "registration";
+        }
+
         System.out.println("Registration: " + registration.getName());
         return "redirect:registration";
     }
--- src/main/java/com/pluralsight/Conference/model/Registration.java
+++ src/main/java/com/pluralsight/Conference/model/Registration.java
@@ -1,8 +1,11 @@
 package com.pluralsight.Conference.model;
 
+import jakarta.validation.constraints.NotEmpty;
+
 public class Registration {
     private String name;
 
+    @NotEmpty
     public String getName() {
         return name;
     }
